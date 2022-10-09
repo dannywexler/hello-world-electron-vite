a5dd38c260a2edb50a12e4e43b3edd673d9578fb
Danny Wexler <dannywexler@gmail.com>
2022-10-08 17:52:07 -0700

    added Inter as the global font, and added primeflex link in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import 'primeicons/primeicons.css'
+import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/arya-blue/theme.css'
 
 export default function App() {
-    
+
     return (
         <div className='flex'>
-            Hello world
-            
+            Hello World
         </div>
     )
 }
