8fdad912d37cbdc8685af9ea294690436e4b9f10
Danny Wexler <dannywexler@gmail.com>
2022-10-09 20:52:27 -0700

    added counter with basic useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import 'primeicons/primeicons.css'
 import 'primereact/resources/primereact.min.css'
 import 'primereact/resources/themes/arya-blue/theme.css'
+import { Button } from 'primereact/button';
+
+import { useState } from 'react'
 
 export default function App() {
+    const [count, setCount] = useState(0)
+
+    const increaseBy = (amount: number) => setCount(prev => prev + amount)
 
     return (
         <div className='flex'>
-            Hello World
+            <Button
+                className='mr-2'
+                label='+'
+                onClick={() => increaseBy(2)}
+            />
+            {` Current count is: ${count}`}
         </div>
     )
 }
