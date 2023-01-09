d56167da097790b98d6a281726003fe0004b8d8b
Danny Wexler <dannywexler@gmail.com>
2023-01-09 03:49:44 -0800

    greatly simplified the state selection process, including nested state

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,29 @@ import 'primeicons/primeicons.css'
 import 'primereact/resources/primereact.min.css'
 import 'primereact/resources/themes/arya-blue/theme.css'
 import { Button } from 'primereact/button';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from './rematchStore';
-import { Dispatch, countActions } from './rematchStore'
+import {
+    countActions,
+    useCountState,
+    useNestedState,
+    wait,
+} from './rematchStore';
 
 export default function App() {
-    const count = useSelector((state: RootState) => state.count.count)
-    // const dispatch = useDispatch<Dispatch>()
+    const { currentCountValue } = useCountState()
+    const nested = useNestedState()
 
     return (
         <div className='flex'>
             <Button
                 className='mr-2'
                 label='+'
-                // onClick={() => dispatch.count.increment(13)}
-                onClick={() => countActions.increment(5)}
+                onClick={async () => {
+                    // await wait(1000)
+                    countActions.increment(5);
+                }}
             />
-            {` Current count is: ${count}`}
+            {` Current count is: ${currentCountValue}`}
+            {` Nested item is: ${nested}`}
         </div>
     )
 }
--- src/rematchStore.ts
+++ src/rematchStore.ts
@@ -1,41 +1,48 @@
 import { createModel, init, Models, RematchDispatch, RematchRootState } from "@rematch/core";
 import immerPlugin from "@rematch/immer"
-export interface RootModel extends Models<RootModel> {
-    count: typeof countModel
-}
+import { useSelector } from "react-redux";
+
+export const wait = (ms: number) =>
+	new Promise((resolve) => setTimeout(resolve, ms))
+
 
 export const countModel = createModel<RootModel>()({
     state: {
-        count: 0,
+        currentCountValue: 0,
         some: {
             deeply: {
-                nesting: {
+                nested: {
                     item: true
                 }
             }
         },
     },
     reducers: {
-        increment(state, payload: number) {
-            state.count += payload;
-            state.some.deeply.nesting.item = !state.some.deeply.nesting.item
+        increment(countState, incrementBy: number) {
+            countState.currentCountValue += incrementBy;
+            countState.some.deeply.nested.item = !countState.some.deeply.nested.item
         },
     },
     effects: (dispatch) => ({
-        async incrementAsync(payload: number, state) {
-            console.log("This is current root state", state);
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            dispatch.increment(payload);
+        async incrementAsync(incrementBy: number, countState) {
+            await wait(2000)
+            dispatch.increment(incrementBy);
         },
     }),
 });
 
+export interface RootModel extends Models<RootModel> {
+    count: typeof countModel
+}
+export type RootState = RematchRootState<RootModel>;
 export const models: RootModel = { count: countModel }
 
 export const store = init({
     name: 'Counter store',
     models,
-    plugins: [immerPlugin()],
+    plugins: [
+        immerPlugin(),
+    ],
     redux: {
         devtoolOptions: {
             trace: true
@@ -45,6 +52,10 @@ export const store = init({
 
 export const countActions = store.dispatch.count
 
-export type Store = typeof store;
-export type Dispatch = RematchDispatch<RootModel>;
-export type RootState = RematchRootState<RootModel>;
+export function useCountState () {
+    return useSelector((state: RootState) => state.count)
+}
+
+export function useNestedState() {
+    return useCountState().some.deeply.nested.item
+}
