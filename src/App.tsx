e21f4157218185c699f3449d1d2e62ac3e0b2825
Danny Wexler <dannywexler@gmail.com>
2023-01-09 01:20:34 -0800

    added nested state, immer and simplified dispatch actions

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,19 @@ import 'primereact/resources/themes/arya-blue/theme.css'
 import { Button } from 'primereact/button';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from './rematchStore';
-import { Dispatch } from './rematchStore'
+import { Dispatch, countActions } from './rematchStore'
 
 export default function App() {
-    const count = useSelector((state: RootState) => state.count)
-    const dispatch = useDispatch<Dispatch>()
+    const count = useSelector((state: RootState) => state.count.count)
+    // const dispatch = useDispatch<Dispatch>()
 
     return (
         <div className='flex'>
             <Button
                 className='mr-2'
                 label='+'
-                onClick={() => dispatch.count.increment(2)}
+                // onClick={() => dispatch.count.increment(13)}
+                onClick={() => countActions.increment(5)}
             />
             {` Current count is: ${count}`}
         </div>
--- src/rematchStore.ts
+++ src/rematchStore.ts
@@ -1,32 +1,49 @@
 import { createModel, init, Models, RematchDispatch, RematchRootState } from "@rematch/core";
+import immerPlugin from "@rematch/immer"
 export interface RootModel extends Models<RootModel> {
-    count: typeof count
+    count: typeof countModel
 }
 
-export const count = createModel<RootModel>()({
-    state: 0,
+export const countModel = createModel<RootModel>()({
+    state: {
+        count: 0,
+        some: {
+            deeply: {
+                nesting: {
+                    item: true
+                }
+            }
+        },
+    },
     reducers: {
         increment(state, payload: number) {
-            return state + payload;
+            state.count += payload;
+            state.some.deeply.nesting.item = !state.some.deeply.nesting.item
         },
     },
     effects: (dispatch) => ({
         async incrementAsync(payload: number, state) {
             console.log("This is current root state", state);
             await new Promise((resolve) => setTimeout(resolve, 1000));
-            dispatch.count.increment(payload);
+            dispatch.increment(payload);
         },
     }),
 });
 
-export const models: RootModel = { count }
+export const models: RootModel = { count: countModel }
 
-export const store = init(
-    {
-        name: 'Counter store',
-        models,
+export const store = init({
+    name: 'Counter store',
+    models,
+    plugins: [immerPlugin()],
+    redux: {
+        devtoolOptions: {
+            trace: true
+        }
     }
-);
+});
+
+export const countActions = store.dispatch.count
 
 export type Store = typeof store;
 export type Dispatch = RematchDispatch<RootModel>;
